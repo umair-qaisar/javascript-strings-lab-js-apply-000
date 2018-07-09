3a7ace924f5f69910918b511fab47b31dfc349cb
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-09 07:39:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ var conversation = 'Let\'s talk about space.';*/
 
 let greeting = "Hello, everybody!";
 
-let specialGuest = "Neil deGrasse Tyson"
+let specialGuest = "Neil deGrasse Tyson";
 
-let greetSpecialGuest = `Hello, ${specialGuest}!`
+let greetSpecialGuest = `Hello, ${specialGuest}!`;
+
+let topic = "space";
+
+let conversation = `Let\'s talk about ${topic}.`
+
